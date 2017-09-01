a511475869c823d0c47602069c2a6e2835106d82
Joao Simas <j.eduardo.simas@hotmail.com>
2017-09-01 16:09:42 -0300

    test CI

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,8 +6,8 @@ require "cpf_faker"
 require "rspec"
 
 Capybara.register_driver :selenium do |app|
-  #Capybara::Selenium::Driver.new(app, :browser => :chrome)
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  #Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
 Capybara.default_driver = :selenium
