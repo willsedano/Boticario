1be76bd8fd6d795357e6158aa5af21ffccc03923
Joao Simas <j.eduardo.simas@hotmail.com>
2017-10-20 10:03:35 -0200

    headless chrome 2

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,19 @@ require "faker"
 require "cpf_faker"
 require "rspec"
 
+#ABRE BROWSER
+#Capybara.register_driver :selenium do |app|
+  #Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  #Capybara::Selenium::Driver.new(app, :browser => :firefox)
+#end
+
+#Capybara.default_driver = :selenium
+#Capybara.default_max_wait_time = 10
+
+#Capybara.configure do |config|
+#    config.app_host = 'https://youse.com.br'
+#end
+
 
 #NAO ABRE BROWSER
 Capybara.register_driver :selenium do |app|
@@ -20,5 +33,5 @@ end
 
 Capybara.configure do |config|
     config.default_driver = :selenium
-    config.app_host = 'https://www.com.bryouse.com.br'
+    config.app_host = 'https://youse.com.br'
 end
