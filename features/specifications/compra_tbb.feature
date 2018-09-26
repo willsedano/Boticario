85fe42ed1725ed9dbec8c0917239a745baa1de91
sao paulo <sao paulo@dbs-note-280.dbserver.com.br>
2018-09-26 10:50:32 -0300

    pedido tbb

--- features/specifications/compra_tbb.feature
+++ features/specifications/compra_tbb.feature
@@ -4,13 +4,13 @@ Funcionalidade: Fluxo de compra TBB
 
 Cenario: Entrar no site thebeautybox e fechar um pedido
 
-    Dado que eu acesse o site thebeautybox
-	E que selecione um produto
+    Dado que acesse o site thebeautybox
+	Dado que escolha um produto
 	E que selecione comprar agora
-	Dado que selecione fechar pedido
-	Quando informar o email
-	E continuar comprando
-	Quando ir em pagamento
-	Quando selecionar boleto bancario
-	Entao finalizar compra
+	Dado que escolha fechar pedido
+	Quando digitar email no campo
+	E click em continuar comprando
+	Quando selecionar ir em pagamento
+	E selecionar opcao boleto bancario
+	Entao finalize sua compra
 	
--- features/step_definitions/compra_tbb.rb
+++ features/step_definitions/compra_tbb.rb
@@ -1,15 +1,73 @@
+Dado(/^que acesse o site thebeautybox$/) do
+  visit 'https://www.thebeautybox.com.br'
+  page.execute_script "window.scrollBy(0,10000)"
+end
+
+Dado("que escolha um produto") do
+  find("a[href='https://www.thebeautybox.com.br/mascara-facial-controla-poros-missha-real-solution-pore-control_820384/p']").click
+   
+end
+
+E("que selecione comprar agora") do 
+  sleep 8
+page.execute_script "window.scrollBy(0,10000)" 
+  find('a.buy-button.buy-button-ref').click
+  #href="#/orderform"
+
+end
+
+Dado("que escolha fechar pedido") do  
+ page.execute_script "window.scrollBy(0,10000)"
+  sleep 8
+  find('a[href="#/orderform"]').click
+
+end
+
+Quando("digitar email no campo") do
+  sleep 8
+  #page.execute_script "window.scrollBy(0,10000)"
+ #fill_in(:id, 'client-pre-email').set('williamsedano@hotmail.com') 
+ fill_in "client-pre-email", :with => "williamsedano@hotmail.com"   
+  find('.btn-success span').click 
+  #find('button#btn-identified-user-button').click 
+end
+
+E("click em continuar comprando") do 
+  sleep 8
+  find('.identified-user-modal-body button').click 
+end
+
+
+Quando("selecionar ir em pagamento") do 
+page.execute_script "window.scrollBy(0,10000)"
+  sleep 8 
+  find('button.submit.btn-go-to-payment.btn.btn-large.btn-success').click 
+end
+
+E("selecionar opcaoboleto bancario") do  
+page.execute_script "window.scrollBy(0,10000)"
+  sleep 8 
+  find('#payment-group-bankInvoicePaymentGroup').click 
+  sleep 8
+end
+
+Entao("finalizar compra") do 
+page.execute_script "window.scrollBy(0,10000)"
+  sleep 8  
+  find('.btn-success span').click   
+end
+
 Dado(/^que eu acesse o site thebeautybox$/) do
   visit 'https://www.thebeautybox.com.br'
   page.execute_script "window.scrollBy(0,10000)"
 end
 
 Dado("que selecione um produto") do
-  find("a[href='/deo-colonia-eudora-kiss-me-cabernet-50ml_819217/p']").click
+  find("a[href='https://www.thebeautybox.com.br/mascara-facial-controla-poros-missha-real-solution-pore-control_820384/p']").click
    
 end
 
-E("que selecione comprar agora") do  
- page.execute_script "window.scrollBy(0,10000)"
+E("que selecione botao comprar agora") do  
   sleep 8
   find('a.buy-button.buy-button-ref').click
   #href="#/orderform"
@@ -44,16 +102,15 @@ page.execute_script "window.scrollBy(0,10000)"
   find('button.submit.btn-go-to-payment.btn.btn-large.btn-success').click 
 end
 
-Quando("selecionar boleto bancario") do  
+E("selecionar opcao boleto bancario") do  
 page.execute_script "window.scrollBy(0,10000)"
   sleep 8 
   find('#payment-group-bankInvoicePaymentGroup').click 
   sleep 8
 end
 
-Entao("finalizar compra") do 
+Entao("finalize sua compra") do 
 page.execute_script "window.scrollBy(0,10000)"
   sleep 8  
   find('.btn-success span').click   
 end
-
